e7a6fbf9e01d6b985ca683580a371d13bf576786
James Sunseri <jamessunseri@berkeley.edu>
2024-03-12 16:59:35 -0400

    second commit

--- PyCosmoMMF/__init__.py
+++ PyCosmoMMF/__init__.py
@@ -6,3 +6,10 @@ from PyCosmoMMF.hessian import *
 from PyCosmoMMF.tagging import *
 from PyCosmoMMF.utils import *
 from PyCosmoMMF.signatures import *
+
+
+import pkg_resources
+test_field = np.load(pkg_resources.resource_filename(__name__, 'test_density_cube.npy'))
+
+
+
